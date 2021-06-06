04d3f059df445dd0decb9d3702a01ec47d69a5a6
Vansh018 <75937381+Vansh018@users.noreply.github.com>
2021-06-06 17:31:20 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+noseX = 0;
+noseY = 0;
+difference = 0;
+rightWristX = 0;
+leftWristY = 0;
+
 function setup(){
     video = createCapture(VIDEO);
     video.size(550, 500);
@@ -7,4 +13,20 @@ function setup(){
 
     poseNet = ml5.poseNet(video, modelLoaded);
     poseNet.on('pose', gotPoses);
+}
+
+function draw(){
+    background("red");
+}
+
+function modelLoaded(){
+    console.log("PoseNet Is Initialized");
+}
+
+function gotPoses(results){
+    if(results.length > 0)
+    {
+        console.log(results);
+        
+        }
 }
